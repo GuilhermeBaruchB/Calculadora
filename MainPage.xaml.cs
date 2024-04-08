b30e1fa465e677d5c36197412646afb0d8b9be67
Usuario <Usuario@CPD03-M16>
2024-04-08 14:56:48 -0300

    ajuste

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,105 +9,10 @@
             InitializeComponent();
         }
 
-
-        private void multiplicar_Clicked(System.Object sender, System.EventArgs e)
         {
 
-        }
 
-        private void multiplicar_Clicked_1(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num9_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void Zerar_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void maismenos_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void porcento_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void dividir_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num7_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num8_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num4_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num5_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num6_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void subtracao_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num1_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num2_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num3_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void adicao_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void num0_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void ponto_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void igual_Clicked(System.Object sender, System.EventArgs e)
-        {
-
+            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 
